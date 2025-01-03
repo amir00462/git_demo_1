24b0b8edd2dfc570cd98af2e74e7b2e4da01d713
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-01-03 08:04:16 +0330

    Change Title of app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yaghoot Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
